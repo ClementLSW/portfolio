e86bba8d490518d22191dbd48fface6c2f2fcbc2
Clement Leow <clementlsw@gmail.com>
2024-03-19 11:22:54 +0800

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -94,7 +94,7 @@ export default function Projects({ color }) {
               </Fade>
             ))}
           </Stack>
-          <!--
+          /*
           <Text color={"gray.600"} fontSize={"xl"} px={4}>
             Other Projects
           </Text>
@@ -164,7 +164,7 @@ export default function Projects({ color }) {
                 </Fade>
               ))}
           </SimpleGrid>
-          -->
+          -*/
         </Stack>
       </Container>
     </>
